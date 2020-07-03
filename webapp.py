3db409243475eec434507537153724f615a5957a
Robert <rjconstable1989@gmail.com>
2020-07-03 17:58:42 +0100

    many plots, checkboxes too

--- webapp.py
+++ webapp.py
@@ -1,15 +1,20 @@
 #https://www.youtube.com/watch?v=zK4Ch6e1zq8
 import streamlit as st
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
 import analysis
-from pandas_profiling import ProfileReport 
+from pandas_profiling import ProfileReport
+import altair as alt
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
 st.title("Auto-EDA")
 st.write('A tool for exploratory data analysis')
 st.write('https://github.com/rjc89/Auto_EDA/blob/master/README.md')
-#uploaded_file = st.file_uploader("Choose a dataset", type="csv")
-uploaded_file = "/home/robert/Documents/Auto_EDA/Q3-data.csv"
 
+uploaded_file = st.file_uploader("Choose a dataset .csv", type="csv")
+#uploaded_file = "/home/robert/Documents/Auto_EDA/Q3-data.csv"
+#uploaded_file = "/home/robert/Documents/Auto_EDA/housing.csv"
 # provide housing.csv as a test dataset
 if uploaded_file is not None:
     st.sidebar.title('Is this a time series dataset?')
@@ -27,41 +32,66 @@ if uploaded_file is not None:
     cols = list(data.columns)
     cols.append(None)
 
+# TIME SERIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     if ts_choice == 'Yes':
         t_col = st.sidebar.selectbox('Select time/date column', cols)
         st.sidebar.title('Select Plot Variables')
-        selection = st.sidebar.selectbox('Select Variable', cols)
-        #st.pyplot(analysis.plot_timeseries(data, t_col, selection))
-        st.line_chart(data) 
-        #st.altair_chart(analysis.plot_timeseries(data, t_col, selection))
-        # add overlay time series button
+        selection = st.sidebar.multiselect('', cols[:-1])
 
-    else:
-        # plot histograms of variables
+        st.sidebar.title('Group and Color By')
+        colour_by = st.sidebar.selectbox('', cols)
+        st.title('Time Series')
+        st.subheader('Selection Overlay')    
+        st.line_chart(data[selection])
+         
+        if colour_by == None:
+            st.subheader('Group and Color By')
+            st.write('***<---Select variable to group and color by***') 
+            
+        else:
+            st.subheader('Group and Color By') 
+            for i in selection:
+                if i == t_col:
+                    pass
+                else:
+                    c = alt.Chart(data).mark_line().encode(x = t_col+':Q', y = i+':Q',
+                    tooltip = cols[:-1], color = colour_by+':N').interactive()
+                    st.altair_chart(c, use_container_width=True)
         
-        # st.title('Histograms')
-        # st.pyplot(analysis.plot_histograms(data))
+# OTHER DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    else:
         # selectbar for selecting target variables
         st.sidebar.title('Select Target Variable')
         target = st.sidebar.multiselect('Target', cols) 
 
         # sidebar for selecting categorical variables
-        st.sidebar.title('Deselect Plot Variables')
-        deselection = st.sidebar.multiselect('Deselection', cols)
+        st.sidebar.title('Select Plot Variables')
+        selection = st.sidebar.multiselect('', cols)
 
-        st.sidebar.title('Colour By Variable')
-        colour_by = st.sidebar.selectbox('colour by', cols)    
+        st.sidebar.title('Colour By')
+        colour_by = st.sidebar.selectbox('', cols)    
         
-        #plotting a pair grid
-        st.title('Pair Grid')
-        st.pyplot(analysis.plot_pairgrid(data.loc[:, cols != deselection], colour_by))
-        #st.pyplot(analysis.plot_histograms(data.loc[:, cols != deselection]))
-        #st.line_chart(data) 
-        
-        # Plotting correlation plot of variables with specified R^2 cutoff
+        #st.title('Distributions')
 
-        # plotting pairs of variables of interest
-        # st.title('Explore Pairwise')
-        # st.write('Select variables')
-        # st.altair_chart(selection)
-    
+        show_dists = st.checkbox('Explore Distributions')
+        if show_dists:
+            st.subheader('Histograms')
+            # plot histograms of selected variables
+            plot_all = st.checkbox('Plot All Data')
+            if plot_all:
+                selection = cols[:-1]
+            for i in selection:
+                #bin_slider = st.slider('bin size')
+                if is_numeric_dtype(data[i]):
+                    c = alt.Chart(data).mark_bar().encode(alt.X(i+":Q",
+                     bin=alt.Bin(extent=[0, data[i].max()], step=data[i].max()/50)),
+                    y='count()',).interactive()
+                    st.altair_chart(c, use_container_width=True)
+                else:
+                    pass
+        else:
+            pass
+
+        show_pairwise = st.checkbox('Explore Pair Plots')
+        if show_pairwise:
+            st.subheader('Explore Pairwise')
